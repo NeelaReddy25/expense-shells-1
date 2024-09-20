6da8f8722938e902fefbec6e727c2672dacd26c5
neelareddy <neelareddy.i0204@gmail.com>
2024-09-20 10:54:33 +0530

    script

--- backend.sh
+++ backend.sh
@@ -40,7 +40,7 @@ npm install &>>$LOGFILE
 VALIDATE $? "Installing nodejs dependencies"
 
 #check your repo and path
-cp /home/ec2-user/expense-shells/backend.service /etc/systemd/system/backend.service &>>$LOGFILE
+cp /home/ec2-user/expense-shells-1/backend.service /etc/systemd/system/backend.service &>>$LOGFILE
 VALIDATE $? "Copied backend.service"
 
 systemctl daemon-reload &>>$LOGFILE
